e3b05d7c6d12a989b8a86461130f85d3039c2fd4
loyada <dan.loya@gmail.com>
2020-06-28 14:35:58 -0400

    update version to 1.00

--- setup.py
+++ setup.py
@@ -29,9 +29,9 @@ setup(
     license="MIT",
     long_description=long_description,
     url="http://github.com/loyada/typedpy",
-    download_url ="https://github.com/loyada/typedpy/archive/v0.62.tar.gz",
+    download_url ="https://github.com/loyada/typedpy/archive/v1.00.tar.gz",
     keywords=['testing', 'type-safe', 'strict', 'schema', 'validation'],
-    version='0.62'
+    version='1.00'
 )
 
 # coverage run --source=typedpy/ setup.py test

ae508ba6c3d2eb798cc503c87f74cb386f5d106e
Danny Loya <dan.loya@gmail.com>
2020-07-30 14:02:36 -0400

    doc

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 
 from setuptools import setup
 
-with open(os.path.join(os.path.dirname(__file__), "README.rst")) as readme:
+with open(os.path.join(os.path.dirname(__file__), "README.md")) as readme:
     long_description = readme.read()
 
 classifiers = [

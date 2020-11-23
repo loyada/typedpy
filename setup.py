693348d01683c0dbfc0df1c1d6536e606d155f57
loyada <dan.loya@gmail.com>
2020-11-23 17:20:43 -0500

    version 2.0.1

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     url="http://github.com/loyada/typedpy",
     download_url ="https://github.com/loyada/typedpy/archive/v1.50.tar.gz",
     keywords=['testing', 'type-safe', 'strict', 'schema', 'validation'],
-    version='2.0.0'
+    version='2.0.1'
 )
 
 # coverage run --source=typedpy/ setup.py test

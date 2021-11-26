e36288ae789f238b6de1de8523f33df443bebb30
loyada <dan.loya@gmail.com>
2021-11-26 01:25:38 -0500

    partial - basic functionality works

--- tests/test_partial.py
+++ tests/test_partial.py
@@ -23,3 +23,16 @@ def test_partial_of_structure():
     assert bar.x == "xyz"
 
 
+def test_direct_assignment_to_partial():
+    Bar = Partial[Foo]
+    assert not issubclass(Bar, Foo)
+    assert issubclass(Bar, Structure)
+    bar = Bar(i=5, a={1,2})
+    assert bar.i == 5
+    assert bar.s is None
+    assert bar.a == {2,1}
+
+
+def test_serialization_mapper_of_partial():
+    pass
+

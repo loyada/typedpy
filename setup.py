253b40c20115b5706d86cda2ac935885977dd23c
loyada <dan.loya@gmail.com>
2021-12-12 19:46:13 -0500

    2.8

--- setup.py
+++ setup.py
@@ -29,9 +29,9 @@ setup(
     license="MIT",
     long_description=long_description,
     url="http://github.com/loyada/typedpy",
-    download_url ="https://github.com/loyada/typedpy/archive/v2.7.3.tar.gz",
+    download_url ="https://github.com/loyada/typedpy/archive/v2.8.0.tar.gz",
     keywords=['testing', 'type-safe', 'strict', 'schema', 'validation'],
-    version='2.7.3'
+    version='2.8.0'
 )
 
 # coverage run --source=typedpy/ setup.py test

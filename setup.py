92cdc7b0f8cd7ec5439b184a0f2086ab3ebf5a6b
loyada <dan.loya@gmail.com>
2022-10-05 10:37:00 -0400

    2.19.4

--- setup.py
+++ setup.py
@@ -44,9 +44,9 @@ setup(
     license="MIT",
     long_description=long_description,
     url="http://github.com/loyada/typedpy",
-    download_url="https://github.com/loyada/typedpy/archive/v2.19.3.tar.gz",
+    download_url="https://github.com/loyada/typedpy/archive/v2.19.4.tar.gz",
     keywords=["testing", "type-safe", "strict", "schema", "validation"],
-    version="2.19.3",
+    version="2.19.4",
 )
 
 # coverage run --source=typedpy/ setup.py test

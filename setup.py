ded3bba2dd1ab41eb0ba8bf2e00e2c34ba34d110
Danny Lya <dan.loya@gmail.com>
2024-09-22 14:56:48 -0400

    bump version

--- setup.py
+++ setup.py
@@ -46,9 +46,9 @@ setup(
     license="MIT",
     long_description=long_description,
     url="http://github.com/loyada/typedpy",
-    download_url="https://github.com/loyada/typedpy/archive/v2.28.1.tar.gz",
+    download_url="https://github.com/loyada/typedpy/archive/v2.28.2.tar.gz",
     keywords=["testing", "type-safe", "strict", "schema", "validation"],
-    version="2.28.1",
+    version="2.28.2",
 )
 
 # coverage run --source=typedpy/ setup.py test
